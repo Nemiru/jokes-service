7676eb99587ae69dd638b7c4080ff01557bc7d1a
Nemiru <blankwashere@protonmail.com>
2023-01-25 17:45:05 +0000

    some changes

--- index.js
+++ index.js
@@ -1,15 +1,26 @@
 const express = require('express');
 const app = express();
 const { Joke } = require('./db');
+const { Op } = require("sequelize")
 
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 
 app.get('/jokes', async (req, res, next) => {
+  console.log(req.query);
+  let jokes = [];
   try {
-    // TODO - filter the jokes by tags and content
-    const jokes = [];
-    res.send(jokes);
+    if(Array.isArray(req.query.tags)) {
+      jokes = await Joke.findAll({where: {tags: req.query.tags }});
+      //res.status(200).console.log(jokes);
+      //return;
+    } else if (req.query.tags) {
+      jokes = await Joke.findAll({where: {tags: req.query.tags }});
+    } else {
+      jokes = await Joke.findAll();
+    }
+        res.send(jokes);
+    //res.send("thanks for stopping by!")
   } catch (error) {
     console.error(error);
     next(error)
